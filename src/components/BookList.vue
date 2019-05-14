66125faee065e85f6203626648aad556eba3d2ab
Kurt Rogiers <kri17lt@admin.colruyt.int>
2019-05-14 14:44:57 +0200

    Completed the module

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -15,7 +15,7 @@
         <book-item v-for='book in filteredBooks' :key='book.id' :book='book'></book-item>
       </ul>
     </div>
-    
+
     <br><hr>
     <book-form @addBook='appendBook'></book-form>
   </div>
@@ -42,6 +42,7 @@ export default {
       searchInput: ""
     };
   },
+  
   components: {
     BookItem,
     BookForm
